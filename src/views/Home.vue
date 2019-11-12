a1915c756d12b42d195cac06f88139450e3f26d7
Daniel Röwenstrunk <roewenstrunk@uni-paderborn.de>
2019-11-12 21:21:29 +0100

    Twitter integration

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,11 +5,11 @@
 
     <div class="container grid-lg mainContent">
       <div class="columns">
-        <div class="column col-8">
+        <div class="column col-12">
           <h1>{{ $t('ZenMEM') }}</h1>
           <p v-html="$t('home_text')"></p>
         </div>
-        <div class="column col-4 newsColumn">
+        <div class="column col-6 col-md-6 col-sm-12 col-xs-12 newsColumn">
           <div class="tile" v-for="(news, index) in News"
                :key="`news${index}`">
             <div class="tile-icon">
@@ -21,6 +21,11 @@
               <p class="tile-title">{{ getNewsByCurrentLang(news).title }}</p>
               <p class="tile-subtitle">{{ getNewsByCurrentLang(news).date }}</p>
             </div>
+          </div>
+        </div>
+        <div class="column col-6 col-md-6 col-sm-12 col-xs-12 twitterColumn">
+          <div class="twitter">
+            <a class="twitter-timeline" data-height="400" data-dnt="true" data-link-color="#335480" href="https://twitter.com/roewenstrunk/lists/zenmem?ref_src=twsrc%5Etfw">A Twitter List by roewenstrunk</a>
           </div>
         </div>
       </div>
@@ -55,6 +60,16 @@ export default {
 <style scoped>
   .home .mainContent {
     margin-top: 5rem;
+  }
+
+  .newsColumn.col-6, .twitterColumn.col-6 {
+    padding-top: 3rem;
+  }
+
+  .twitter {
+    background-color: #f3f3f3;
+    padding: 0.5rem;
+    border-radius: 0.2rem;
   }
 </style>
 
