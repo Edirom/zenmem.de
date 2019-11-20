fba027bcd9df8a875526446f324254733ee1463c
Johannes Kepper <kepper@edirom.de>
2019-11-20 23:59:17 +0100

    adding views

---
+++ src/views/Standards.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="standards">
+
+        <div class="container grid-lg mainContent">
+            <div class="columns">
+                <div class="column col-12">
+                    <h1>{{ $t('standards') }}</h1>
+                    <p v-html="$t('standards_text')"></p>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "standards",
+    }
+</script>
+
+<style scoped>
+    .standards .mainContent {
+        margin-top: 5rem;
+    }
+</style>
---
+++ src/views/Tools.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="tools">
+
+        <div class="container grid-lg mainContent">
+            <div class="columns">
+                <div class="column col-12">
+                    <h1>{{ $t('tools') }}</h1>
+                    <p v-html="$t('tools_text')"></p>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "tools",
+    }
+</script>
+
+<style scoped>
+    .tools .mainContent {
+        margin-top: 5rem;
+    }
+</style>
