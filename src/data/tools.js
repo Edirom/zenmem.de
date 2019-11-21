b65fac2e9e385e8c86ab976985cbe743c958929b
Johannes Kepper <kepper@edirom.de>
2019-11-21 21:31:55 +0100

    adding tools

---
+++ src/data/tools.js
@@ -0,0 +1,114 @@
+export default [
+    {
+        title: 'Edirom Tools',
+        de: {
+            desc: 'Edirom ist ein Werkzeug, welches speziell für digitale Formen wissenschaftlich-kritischer Musikeditionen entwickelt wurde. Die entwickelten Werkzeuge sind dabei als quelloffene Tools sowohl zur Darstellung der Ergebnisse editorischer Arbeit als auch als modular aufgebaute Editorentools konzipiert, die einen effizienten Einsatz neuer Technologien in der Musikphilologie erlauben.'
+        },
+        en: {
+            desc: 'Edirom is a tool which was developed especially for digital forms of scientific-critical music editions. The developed tools are designed as open source tools to display the results of editorial work as well as modular editor tools, which allow an efficient use of new technologies in music philology.'
+        },
+        URL: 'https://github.com/Edirom/Edirom-Online',
+        //logo: '/project-pix/beethoven.png',
+        hotness: 50
+    },
+    {
+        title: 'MEI Score Editor',
+        de: {
+            desc: 'Der <em>MEI Score Editor</em> (<em>MEISE</em>) ist ein plattformunabhängiger Online-MEI-Editor, der eine gleichzeitige Bearbeitung und Darstellung von MEI-Daten ermöglicht und bei der Arbeit mit editorischen Kodierungsprinzipien verwendet werden kann.'
+        },
+        en: {
+            desc: 'The <em>MEI Score Editor</em> (<em>MEISE</em>) is a platform-independent online MEI editor that enables simultaneous editing and display of MEI data and can be used when working with editorial coding principles. '
+        },
+        URL: 'https://github.com/Edirom/MEISE-2',
+        //logo: '/project-pix/hoftheater_text.png',
+        hotness: 10
+    },
+    {
+        title: 'SMuFL Browser',
+        de: {
+            desc: 'Der SMuFL Browser ermöglicht das einfache Einbinden von musikalischen Symbolen in TEI Texte. Dafür dient einerseits der "TEI code for embedding" beim Browsen der Zeichen, andererseits kann eine TEI Beschreibung der Zeichen abgerufen werden.'
+        },
+        en: {
+            desc: 'The SMuFL Browser allows the easy integration of musical symbols into TEI texts. On the one hand the "TEI code for embedding" serves for this when browsing the characters, on the other hand a TEI description of the characters can be retrieved.'
+        },
+        URL: 'https://github.com/Edirom/SMuFL-Browser',
+        //logo: '/project-pix/meiLogo.png',
+        hotness: 55
+    },
+    {
+        title: 'Measure Detector',
+        de: {
+            desc: 'Ein Neuronales Netzwerk, welches automatisch Taktpositionen erkennen und als MEI-Codierung bereitstellen kann. Funktioniert bei Drucken bereits sehr zuverlässig.'
+        },
+        en: {
+            desc: 'A neural network that can automatically detect measure positions in digital facsimiles and encodes them as MEI. Works surprisingly well on music prints.'
+        },
+        URL: 'https://measure-detector.edirom.de/',
+        logo: '/tools-pix/measure-detector.png',
+        hotness: 70
+    },
+    {
+        title: 'VideApp',
+        de: {
+            desc: 'Eine komplexe Anwendung zur Darstellung musikalischer Schreibprozesse, sowohl in Manuskripten als auch in Transkriptionen. Entwickelt im Rahmen von <em>Beethovens Werkstatt</em>.'
+        },
+        en: {
+            desc: 'A complex tool for visualising genetical processes in music manuscripts and transcriptions alike. Developed in the <em>Beethovens Werkstatt</em> project.'
+        },
+        URL: 'https://github.com/BeethovensWerkstatt/VideApp',
+        //logo: '/tools-pix/videapp.png',
+        hotness: 60,
+    },
+    {
+        title: 'eHinman',
+        de: {
+            desc: 'Eine web-basierte Umsetzung des <em><a href="https://en.wikipedia.org/wiki/Hinman_collator" target="_blank">Hinman Collator</a></em> bzw. des Überblende-Modus von <em><a href="http://www.aruspix.net/" target="_blank">Aruspix</a></em>. Gegenwärtig in Entwicklung.'
+        },
+        en: {
+            desc: 'A web-based take on the <em><a href="https://en.wikipedia.org/wiki/Hinman_collator" target="_blank">Hinman Collator</a></em> and the superimposition mode of <em><a href="http://www.aruspix.net/" target="_blank">Aruspix</a></em>. Currently under active development.'
+        },
+        URL: 'https://ehinman.edirom.de/',
+        logo: '',
+        hotness: 65,
+    }/*,
+    {
+        title: 'Verovio',
+        de: {
+            subtitle: '',
+            desc: 'Laufzeit: 1993–2026<br/>Träger: <a href="http://www.adwmainz.de/" target="_blank">Akademie der Wissenschaften und der Literatur | Mainz</a>',
+        },
+        en: {
+            subtitle: '',
+            desc: 'Funding period: 1993–2026<br/>Funding agency: <a href="http://www.adwmainz.de/" target="_blank">Akademie der Wissenschaften und der Literatur | Mainz</a>',
+        },
+        URL: 'https://www.weber-gesamtausgabe.de',
+        logo: '/project-pix/wega.png',
+        ends: 2026,
+    }*/
+];
+
+/*
+
+Verbundstelle
+Sarti
+
+ */
+
+/*
+
+    {
+        title: '',
+        de: {
+            subtitle: '',
+            desc: 'Laufzeit: 20–20<br/>Förderer: <a href="" target="_blank"></a>',
+        },
+        en: {
+            subtitle: '',
+            desc: 'Funding period: 20–20<br/>Funding agency: <a href="" target="_blank"></a>',
+        },
+        URL: '',
+        logo: '',
+        ends: 20,
+    },
+
+ */
--- src/views/Tools.vue
+++ src/views/Tools.vue
@@ -6,6 +6,13 @@
                 <div class="column col-12">
                     <h1>{{ $t('tools') }}</h1>
                     <p v-html="$t('tools_text')"></p>
+
+                    <div class="container grid-lg">
+                        <div class="columns">
+                            <Tool v-for="(tool, index) in orderedTools"
+                                :key="`tool${index}`" :tool="tool"/>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
@@ -13,13 +20,26 @@
 </template>
 
 <script>
+    import Tool from "../components/Tool";
+    import toolsData from "../data/tools";
+
     export default {
         name: "tools",
+        components: {Tool},
+        computed: {
+            orderedTools: function () {
+                return toolsData.sort((a, b) => b.hotness - a.hotness );
+            }
+        }
     }
 </script>
 
 <style scoped>
     .tools .mainContent {
+        margin-top: 5rem;
+    }
+
+    .margin-top {
         margin-top: 5rem;
     }
 </style>
